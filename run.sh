5f7b0ed01ffac38e8b565be261d8e4e8e3de04f0
itsmemarcos <mcsnicolete@gmail.com>
2020-05-25 15:42:15 -0300

    ajust merge conflict

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 app="myflaskapp"
+docker container rm -f ${app}
 docker build -t ${app} .
-docker run -d -p 54321:80 \
+docker run -d -p 8010:80 \
+  -p 54321:80 \
   --name=${app} \
   -v $PWD:/app ${app}
